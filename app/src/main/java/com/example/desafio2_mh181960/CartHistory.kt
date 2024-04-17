60510b3e17a6026d7aca5894ba70978a1e6d38a1
Roberto <robertoher76@gmail.com>
2024-04-16 19:19:11 -0600

    activities

---
+++ app/src/main/java/com/example/desafio2_mh181960/CartHistory.kt
@@ -0,0 +1,11 @@
+package com.example.desafio2_mh181960
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CartHistory : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cart_history)
+    }
+}
