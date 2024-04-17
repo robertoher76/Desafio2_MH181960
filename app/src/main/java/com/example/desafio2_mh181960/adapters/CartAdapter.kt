869c2ed189bbf5aefc03478afebc1efcfca47f34
Roberto <robertoher76@gmail.com>
2024-04-16 20:04:49 -0600

    carrito

--- app/src/main/java/com/example/desafio2_mh181960/adapters/CartAdapter.kt
+++ app/src/main/java/com/example/desafio2_mh181960/adapters/CartAdapter.kt
@@ -1,8 +1,5 @@
 package com.example.desafio2_mh181960.adapters
 
-
-
-
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -16,14 +13,10 @@ import com.example.desafio2_mh181960.models.Medicine
 import com.google.firebase.database.FirebaseDatabase
 class CartAdapter(private val mList: List<Medicine>) : RecyclerView.Adapter<CartAdapter.ViewHolder>() {
     // create new views
-    var context: Context? = null
-    fun CarritoAdapter(context: Context?) {
-        this.context = context
-    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         // inflates the card_view_design view
         // that is used to hold list item
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.medicine_adapter, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.cart_adapter, parent, false)
         return ViewHolder(view)
     }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -36,11 +29,10 @@ class CartAdapter(private val mList: List<Medicine>) : RecyclerView.Adapter<Cart
         return mList.size
     }
 
-
     // Holds the views for adding it to image and text
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val textView: TextView = itemView.findViewById(R.id.textView)
-        val btnAddToCart: Button =  itemView.findViewById(R.id.BtnAddToCart)
+        val btnAddToCart: Button =  itemView.findViewById(R.id.BtnDeleteToCart)
         var medicine = Medicine()
         init {
             btnAddToCart.setOnClickListener(object : View.OnClickListener {
